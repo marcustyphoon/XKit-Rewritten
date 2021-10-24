9382abcd72a05c88f98c6435ccd941e40d6341ef
Marcus <marcustyphoon@gmail.com>
2021-10-24 14:45:10 -0700

    Create hide note counts tweak

---
+++ src/scripts/tweaks/hide_note_counts.js
@@ -0,0 +1,48 @@
+import { keyToClasses } from '../../util/css_map.js';
+import { onNewPosts } from '../../util/mutations.js';
+import { buildStyle } from '../../util/interface.js';
+
+const excludeClass = 'xkit-notes-hidden-done';
+const replacementClass = 'xkit-notes-hidden-replacement';
+const styleElement = buildStyle(`.${replacementClass} + span { display: none; }`);
+
+let containerSelector;
+
+const hideNotes = async function () {
+  [...document.querySelectorAll(containerSelector)]
+    .forEach(element => {
+      element.classList.add(excludeClass);
+      const noteElement = element.querySelector('span');
+      if (!noteElement) return;
+
+      const replacement = noteElement.cloneNode();
+      replacement.classList.add(replacementClass);
+
+      const textWithoutCount = noteElement.innerText
+        .replace(/^[\d,]+ ?/, '')
+        .replace(/ [\d,]+.*/, '');
+      replacement.innerText = textWithoutCount;
+
+      noteElement.before(replacement);
+    });
+};
+
+export const main = async function () {
+  const noteCountContainerClasses = await keyToClasses('noteCountContainer');
+  containerSelector = noteCountContainerClasses
+    .map(className => `.${className}:not(.${excludeClass})`)
+    .join(', ');
+
+  onNewPosts.addListener(hideNotes);
+  hideNotes();
+
+  document.head.append(styleElement);
+};
+
+export const clean = async function () {
+  onNewPosts.removeListener(hideNotes);
+  styleElement.remove();
+
+  $(`.${excludeClass}`).removeClass(excludeClass);
+  $(`.${replacementClass}`).remove();
+};
