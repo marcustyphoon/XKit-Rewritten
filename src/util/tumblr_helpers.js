49b01b2d5ad269c12c004288e2f1222a87da1919
Marcus <marcustyphoon@gmail.com>
2022-02-17 18:05:38 -0800

    Improve warning messages from apiFetch

--- src/util/tumblr_helpers.js
+++ src/util/tumblr_helpers.js
@@ -1,16 +1,19 @@
 import { inject } from './inject.js';
 
+const requestApiFetch = async (resource, init) =>
+  window.tumblr.apiFetch(resource, init)
+    .catch(error => {
+      const consoleWarn = console.warn.__sentry_original__ || console.warn;
+      consoleWarn('XKit Rewritten API fetch was rejected with:', JSON.stringify(error, null, 2));
+      throw error;
+    });
+
 /**
  * @param {...any} args - Arguments to pass to window.tumblr.apiFetch()
  * @see {@link https://github.com/tumblr/docs/blob/master/web-platform.md#apifetch}
  * @returns {Promise<Response|Error>} Resolves or rejects with result of window.tumblr.apiFetch()
  */
-export const apiFetch = async function (...args) {
-  return inject(
-    async (resource, init) => window.tumblr.apiFetch(resource, init),
-    args
-  );
-};
+export const apiFetch = (...args) => inject(requestApiFetch, args);
 
 export const getCssMap = inject(async () => window.tumblr.getCssMap());
 export const getLanguageData = inject(async () => window.tumblr.languageData);
