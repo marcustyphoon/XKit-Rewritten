c7acff700ea1459b6bac059e26702d4255d3cd6a
Marcus <marcustyphoon@gmail.com>
2023-03-09 15:20:28 -0800

    user-info-react-props

--- src/util/user.js
+++ src/util/user.js
@@ -1,11 +1,27 @@
+import { inject } from './inject.js';
 import { apiFetch } from './tumblr_helpers.js';
 
-const fetchedUserInfo = await apiFetch('/v2/user/info').catch(() => ({ response: {} }));
+const unburyUserInfo = async () => {
+  const baseContainerElement = document.getElementById('base-container');
+  const reactKey = Object.keys(baseContainerElement).find(key => key.startsWith('__reactFiber'));
+  let fiber = baseContainerElement[reactKey];
+
+  while (fiber !== null) {
+    const { appContext } = fiber.memoizedProps || {};
+    if (appContext !== undefined) {
+      return appContext.getUserInfo();
+    } else {
+      fiber = fiber.return;
+    }
+  }
+};
 
 /**
  * {object?} userInfo - The contents of the /v2/user/info API endpoint
  */
-export const userInfo = fetchedUserInfo.response.user;
+export const userInfo =
+  (await inject(unburyUserInfo).catch(() => undefined)) ??
+  (await apiFetch('/v2/user/info').catch(() => ({ response: {} })))?.response?.user;
 
 /**
  * {object[]} userBlogs - An array of blog objects the current user has post access to
