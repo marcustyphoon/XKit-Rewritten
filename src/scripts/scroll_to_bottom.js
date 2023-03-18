b5064956a449dd1fa9532f3cd1e517e873189a89
Marcus <marcustyphoon@gmail.com>
2023-03-18 01:06:10 -0700

    scroll-to-bottom-modal

--- src/scripts/scroll_to_bottom.js
+++ src/scripts/scroll_to_bottom.js
@@ -3,8 +3,8 @@ import { translate } from '../util/language_data.js';
 import { pageModifications } from '../util/mutations.js';
 import { buildStyle } from '../util/interface.js';
 
-const scrollToBottomButtonId = 'xkit-scroll-to-bottom-button';
-$(`[id="${scrollToBottomButtonId}"]`).remove();
+const buttonClass = 'xkit-scroll-to-bottom-button';
+$(`.${buttonClass}`).remove();
 const activeClass = 'xkit-scroll-to-bottom-active';
 
 const loaderSelector = `
@@ -13,8 +13,13 @@ ${keyToCss('notifications')} + ${keyToCss('loader')}
 `;
 const knightRiderLoaderSelector = `:is(${loaderSelector}) > ${keyToCss('knightRiderLoader')}`;
 
+const modalScrollContainerSelector = `${keyToCss('drawerContent')} > ${keyToCss('scrollContainer')}`;
+
 let scrollToBottomButton;
+let modalScrollToBottomButton;
 let active = false;
+
+let scrollElement;
 
 const styleElement = buildStyle(`
 .${activeClass} svg use {
@@ -22,20 +27,34 @@ const styleElement = buildStyle(`
 }
 `);
 
-const scrollToBottom = () => {
-  window.scrollTo({ top: document.documentElement.scrollHeight });
-  const loaders = [...document.querySelectorAll(knightRiderLoaderSelector)];
+const getScrollElement = () =>
+  document.querySelector(modalScrollContainerSelector) ||
+  document.documentElement;
 
-  if (loaders.length === 0) {
+const getObserveElement = () =>
+  document.querySelector(modalScrollContainerSelector)?.firstElementChild ||
+  document.documentElement;
+
+const scrollToBottom = () => {
+  scrollElement.scrollTo({ top: scrollElement.scrollHeight });
+
+  const buttonConnected = scrollToBottomButton?.isConnected || modalScrollToBottomButton?.isConnected;
+  const loaders = [...scrollElement.querySelectorAll(knightRiderLoaderSelector)];
+
+  if (!buttonConnected || scrollElement !== getScrollElement() || loaders.length === 0) {
     stopScrolling();
   }
 };
 const observer = new ResizeObserver(scrollToBottom);
 
 const startScrolling = () => {
-  observer.observe(document.documentElement);
+  scrollElement = getScrollElement();
+
+  observer.observe(getObserveElement());
   active = true;
-  scrollToBottomButton.classList.add(activeClass);
+  scrollToBottomButton?.classList.add(activeClass);
+  modalScrollToBottomButton?.classList.add(activeClass);
+
   scrollToBottom();
 };
 
@@ -43,18 +62,11 @@ const stopScrolling = () => {
   observer.disconnect();
   active = false;
   scrollToBottomButton?.classList.remove(activeClass);
+  modalScrollToBottomButton?.classList.remove(activeClass);
 };
 
 const onClick = () => active ? stopScrolling() : startScrolling();
 const onKeyDown = ({ key }) => key === '.' && stopScrolling();
-
-const checkForButtonRemoved = () => {
-  const buttonWasRemoved = document.documentElement.contains(scrollToBottomButton) === false;
-  if (buttonWasRemoved) {
-    if (active) stopScrolling();
-    pageModifications.unregister(checkForButtonRemoved);
-  }
-};
 
 const addButtonToPage = async function ([scrollToTopButton]) {
   if (!scrollToBottomButton) {
@@ -66,7 +78,7 @@ const addButtonToPage = async function ([scrollToTopButton]) {
     scrollToBottomButton.style.marginTop = '0.5ch';
     scrollToBottomButton.style.transform = 'rotate(180deg)';
     scrollToBottomButton.addEventListener('click', onClick);
-    scrollToBottomButton.id = scrollToBottomButtonId;
+    scrollToBottomButton.classList.add(buttonClass);
 
     scrollToBottomButton.classList[active ? 'add' : 'remove'](activeClass);
   }
@@ -74,17 +86,37 @@ const addButtonToPage = async function ([scrollToTopButton]) {
   scrollToTopButton.after(scrollToBottomButton);
   scrollToTopButton.addEventListener('click', stopScrolling);
   document.documentElement.addEventListener('keydown', onKeyDown);
-  pageModifications.register('*', checkForButtonRemoved);
+};
+
+const addModalButtonToPage = async function ([modalScrollToTopButton]) {
+  if (!modalScrollToBottomButton) {
+    const hiddenClasses = keyToClasses('hidden');
+
+    modalScrollToBottomButton = modalScrollToTopButton.cloneNode(true);
+    hiddenClasses.forEach(className => modalScrollToBottomButton.classList.remove(className));
+    modalScrollToBottomButton.removeAttribute('aria-label');
+    modalScrollToBottomButton.style.marginTop = '1ch';
+    modalScrollToBottomButton.style.transform = 'rotate(180deg)';
+    modalScrollToBottomButton.addEventListener('click', onClick);
+    modalScrollToBottomButton.classList.add(buttonClass);
+
+    modalScrollToBottomButton.classList[active ? 'add' : 'remove'](activeClass);
+  }
+
+  modalScrollToTopButton.after(modalScrollToBottomButton);
+  modalScrollToTopButton.addEventListener('click', stopScrolling);
+  document.documentElement.addEventListener('keydown', onKeyDown);
 };
 
 export const main = async function () {
   pageModifications.register(`button[aria-label="${translate('Scroll to top')}"]`, addButtonToPage);
+  pageModifications.register(`button[aria-label="${translate('Back to top')}"]`, addModalButtonToPage);
+
   document.documentElement.append(styleElement);
 };
 
 export const clean = async function () {
   pageModifications.unregister(addButtonToPage);
-  pageModifications.unregister(checkForButtonRemoved);
   stopScrolling();
   scrollToBottomButton?.remove();
   styleElement.remove();
