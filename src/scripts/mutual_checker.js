62f157b1b8322be1286fd3d5fd45d566100216ab
Marcus <marcustyphoon@gmail.com>
2023-05-08 23:29:41 -0700

    mutual-checker-blog-cards

--- src/scripts/mutual_checker.js
+++ src/scripts/mutual_checker.js
@@ -1,11 +1,12 @@
-import { filterPostElements } from '../util/interface.js';
-import { timelineObject } from '../util/react_props.js';
+import { buildStyle, filterPostElements } from '../util/interface.js';
+import { blogData, timelineObject } from '../util/react_props.js';
 import { apiFetch } from '../util/tumblr_helpers.js';
 import { primaryBlogName } from '../util/user.js';
 import { keyToCss } from '../util/css_map.js';
-import { onNewPosts } from '../util/mutations.js';
+import { onNewPosts, pageModifications } from '../util/mutations.js';
 import { dom } from '../util/dom.js';
 import { getPreferences } from '../util/preferences.js';
+import { translate } from '../util/language_data.js';
 
 const mutualIconClass = 'xkit-mutual-icon';
 const hiddenClass = 'xkit-mutual-checker-hidden';
@@ -16,16 +17,52 @@ const regularPath = 'M593 500q0-45-22.5-64.5T500 416t-66.5 19-18.5 65 18.5 64.5T
 const aprilFoolsPath = 'M858 352q-6-14-8-35-2-12-4-38-3-38-6-54-7-28-22-43t-43-22q-16-3-54-6-26-2-38-4-21-2-34.5-8T619 124q-9-7-28-24-29-25-44-34-24-16-47-16t-47 16q-15 9-44 34-19 17-28 24-16 12-29.5 18t-34.5 8q-12 2-38 4-38 3-54 6-28 7-43 22t-22 43q-3 16-6 54-2 26-4 38-2 21-8 34.5T124 381q-7 9-24 28-25 29-34 44-16 24-16 47t16 47q9 15 34 44 17 19 24 28 12 16 18 29.5t8 34.5q2 12 4 38 3 38 6 54 7 28 22 43t43 22q16 3 54 6 26 2 38 4 21 2 34.5 8t29.5 18q9 7 28 24 29 25 44 34 24 16 47 16t47-16q15-9 44-34 19-17 28-24 16-12 29.5-18t34.5-8q12-2 38-4 38-3 54-6 28-7 43-22t22-43q3-16 6-54 2-26 4-38 2-21 8-34.5t18-29.5q7-9 24-28 25-29 34-44 16-24 16-47t-16-47q-9-15-34-44-17-19-24-28-12-16-18-29zm-119 62L550 706q-10 17-26.5 27T488 745l-11 1q-34 0-59-24L271 584q-26-25-27-60.5t23.5-61.5 60.5-27.5 62 23.5l71 67 132-204q20-30 55-38t65 11.5 37.5 54.5-11.5 65z';
 
 const following = {};
-const mutuals = {};
+const followingYou = {};
 
 let showOnlyMutuals;
-let icon;
+let aprilFools;
+
+const styleElement = buildStyle(`
+  svg.xkit-mutual-icon {
+    vertical-align: text-bottom;
+
+    height: 1.23em;
+    margin-top: 0;
+    margin-left: 0;
+    margin-right: 0.5ch;
+  }
+
+  [data-timeline="/v2/timeline/dashboard"] .xkit-mutual-checker-hidden article {
+    display: none;
+  }
+
+  ${keyToCss('blogCardBlogLink')} {
+    display: flex;
+    align-items: end;
+  }
+
+  ${keyToCss('blogCardBlogLink')} svg.xkit-mutual-icon {
+    height: 1.4em;
+  }
+`);
+
+const createIcon = blogName => dom('svg', {
+  xmlns: 'http://www.w3.org/2000/svg',
+  class: mutualIconClass,
+  viewBox: '0 0 1000 1000',
+  fill: aprilFools ? '#00b8ff' : 'rgb(var(--black))'
+}, null, [
+  dom('title', { xmlns: 'http://www.w3.org/2000/svg' }, null, [
+    translate('{{blogNameLink /}} follows you!').replace('{{blogNameLink /}}', blogName)
+  ]),
+  dom('path', { xmlns: 'http://www.w3.org/2000/svg', d: aprilFools ? aprilFoolsPath : regularPath })
+]);
 
 const alreadyProcessed = postElement =>
   postElement.classList.contains(mutualsClass) &&
   postElement.querySelector(`.${mutualIconClass}`);
 
-const addIcons = function (postElements) {
+const processPosts = function (postElements) {
   filterPostElements(postElements, { includeFiltered: true }).forEach(async postElement => {
     if (alreadyProcessed(postElement)) return;
 
@@ -36,63 +73,79 @@ const addIcons = function (postElements) {
     const blogName = blogLink?.textContent;
     if (!blogName) return;
 
-    if (following[blogName] === undefined) {
-      const { blog } = await timelineObject(postElement);
-      if (blogName === blog.name) {
-        following[blogName] = Promise.resolve(blog.followed && !blog.isMember);
-      } else {
-        following[blogName] = apiFetch(`/v2/blog/${blogName}/info`)
-          .then(({ response: { blog: { followed } } }) => followed)
-          .catch(() => Promise.resolve(false));
-      }
-    }
-
-    const followingBlog = await following[blogName];
+    const followingBlog = await getIsFollowing(blogName, postElement);
     if (!followingBlog) { return; }
 
-    if (mutuals[blogName] === undefined) {
-      mutuals[blogName] = apiFetch(`/v2/blog/${primaryBlogName}/followed_by`, { queryParams: { query: blogName } })
-        .then(({ response: { followedBy } }) => followedBy)
-        .catch(() => Promise.resolve(false));
-    }
-
-    const isMutual = await mutuals[blogName];
+    const isMutual = await getIsFollowingYou(blogName);
     if (isMutual) {
       postElement.classList.add(mutualsClass);
-      postAttribution.prepend(icon.cloneNode(true));
+      postAttribution.prepend(createIcon(blogName));
     } else if (showOnlyMutuals) {
       postElement.classList.add(hiddenClass);
     }
   });
 };
 
+const processBlogCardLinks = blogCardLinks =>
+  blogCardLinks.forEach(async blogCardLink => {
+    const blogName = blogCardLink.querySelector(keyToCss('blogLinkShort'))?.textContent || blogCardLink?.textContent;
+    if (!blogName) return;
+
+    const followingBlog = await getIsFollowing(blogName, blogCardLink);
+    if (!followingBlog) return;
+
+    const isMutual = await getIsFollowingYou(blogName);
+    if (isMutual) {
+      const icon = createIcon(blogName);
+      !aprilFools && icon.setAttribute('fill', blogCardLink.style.color);
+      blogCardLink.before(icon);
+    }
+  });
+
+const getIsFollowing = async (blogName, element) => {
+  const blog = await blogData(element) ?? (await timelineObject(element))?.blog;
+
+  if (following[blogName] === undefined) {
+    if (blogName === blog?.name) {
+      following[blogName] = Promise.resolve(blog.followed && !blog.isMember);
+    } else {
+      following[blogName] = apiFetch(`/v2/blog/${blogName}/info`)
+        .then(({ response: { blog: { followed } } }) => followed)
+        .catch(() => Promise.resolve(false));
+    }
+  }
+  return following[blogName];
+};
+
+const getIsFollowingYou = (blogName) => {
+  if (followingYou[blogName] === undefined) {
+    followingYou[blogName] = apiFetch(`/v2/blog/${primaryBlogName}/followed_by`, { queryParams: { query: blogName } })
+      .then(({ response: { followedBy } }) => followedBy)
+      .catch(() => Promise.resolve(false));
+  }
+  return followingYou[blogName];
+};
+
 export const main = async function () {
   if (primaryBlogName === undefined) return;
+  document.documentElement.append(styleElement);
 
   ({ showOnlyMutuals } = await getPreferences('mutual_checker'));
   following[primaryBlogName] = Promise.resolve(false);
 
   const today = new Date();
-  const aprilFools = (today.getMonth() === 3 && today.getDate() === 1);
+  aprilFools = (today.getMonth() === 3 && today.getDate() === 1);
 
-  icon = dom('svg', {
-    xmlns: 'http://www.w3.org/2000/svg',
-    class: mutualIconClass,
-    viewBox: '0 0 1000 1000',
-    fill: aprilFools ? '#00b8ff' : 'rgb(var(--black))'
-  }, null, [
-    dom('path', { xmlns: 'http://www.w3.org/2000/svg', d: aprilFools ? aprilFoolsPath : regularPath })
-  ]);
-
-  onNewPosts.addListener(addIcons);
+  onNewPosts.addListener(processPosts);
+  pageModifications.register(`${keyToCss('blogCard')} ${keyToCss('blogCardBlogLink')} > a`, processBlogCardLinks);
 };
 
 export const clean = async function () {
-  onNewPosts.removeListener(addIcons);
+  onNewPosts.removeListener(processPosts);
+  pageModifications.unregister(processBlogCardLinks);
+  styleElement.remove();
 
   $(`.${mutualsClass}`).removeClass(mutualsClass);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
   $(`.${mutualIconClass}`).remove();
 };
-
-export const stylesheet = true;
