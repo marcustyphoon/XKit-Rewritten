f999d8af9564db29a82ecef0cdc6175b7fc22bda
Marcus <marcustyphoon@gmail.com>
2023-07-13 09:01:28 -0700

    [personal] log css map missing keys

--- src/util/css_map.js
+++ src/util/css_map.js
@@ -2,11 +2,25 @@ import { inject } from './inject.js';
 
 export const cssMap = await inject(async () => window.tumblr.getCssMap());
 
+const missingKeys = new Set();
+
 /**
  * @param {...string} keys - One or more element source names
  * @returns {string[]} An array of generated classnames from the CSS map
  */
-export const keyToClasses = (...keys) => keys.flatMap(key => cssMap[key]).filter(Boolean);
+export const keyToClasses = (...keys) => {
+  keys.filter(key => !cssMap[key]).forEach(key => missingKeys.add(key));
+  return keys.flatMap(key => cssMap[key]).filter(Boolean);
+};
+
+setTimeout(async () => {
+  if (missingKeys.size) {
+    const message = `XKit Rewritten missing css keys: ${[...missingKeys].join(', ')}`;
+    console.log(message);
+    const { notify } = await import(browser.runtime.getURL('/util/notifications.js'));
+    notify(message);
+  }
+}, 1000);
 
 /**
  * @param {...string} keys - One or more element source names
