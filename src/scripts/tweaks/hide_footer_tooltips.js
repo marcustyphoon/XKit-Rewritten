8a81d420d737831897d77387f5789763fcd97d27
Marcus <marcustyphoon@gmail.com>
2023-09-10 01:36:22 -0700

    tooltips

--- src/scripts/tweaks/hide_footer_tooltips.js
+++ src/scripts/tweaks/hide_footer_tooltips.js
@@ -1,9 +1,11 @@
+import { tooltipClass } from '../../util/control_buttons.js';
 import { keyToCss } from '../../util/css_map.js';
 import { buildStyle } from '../../util/interface.js';
 
 const styleElement = buildStyle(`
 article footer ${keyToCss('controlIcon')} ${keyToCss('tooltip')},
-article footer ${keyToCss('controlIcon')} ${keyToCss('tooltip')} ~ * {
+article footer ${keyToCss('controlIcon')} ${keyToCss('tooltip')} ~ *,
+.${tooltipClass} {
   display: none;
 }
 `);
