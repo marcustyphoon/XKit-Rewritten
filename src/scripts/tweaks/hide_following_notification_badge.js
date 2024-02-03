047caba474b0b44e9af02d7676888528164ffacb
Marcus <marcustyphoon@gmail.com>
2024-02-03 00:26:14 -0800

    adjust title with observer
    Co-Authored-By: April Sylph <28949509+AprilSylph@users.noreply.github.com>

--- src/scripts/tweaks/hide_following_notification_badge.js
+++ src/scripts/tweaks/hide_following_notification_badge.js
@@ -1,14 +1,43 @@
 import { keyToCss } from '../../util/css_map.js';
+import { dom } from '../../util/dom.js';
 import { buildStyle } from '../../util/interface.js';
 import { translate } from '../../util/language_data.js';
 
 const followingHomeButton = `:is(li[title="${translate('Home')}"], button[aria-label="${translate('Home')}"], a[href="/dashboard/following"])`;
 
+const titleSelector = 'title:not([data-xkit])';
+
+const customTitleElement = dom('title', { 'data-xkit': true });
 const styleElement = buildStyle(`
 ${followingHomeButton} ${keyToCss('notificationBadge')} {
   display: none;
 }
 `);
 
-export const main = async () => document.documentElement.append(styleElement);
-export const clean = async () => styleElement.remove();
+const updateTitle = titleElement => {
+  const rawTitle = titleElement.textContent;
+  const newTitle = rawTitle.replace(/^\(\d{1,2}\) /, '');
+  customTitleElement.textContent = newTitle;
+};
+
+const observer = new MutationObserver(mutations =>
+  mutations
+    .flatMap(({ addedNodes }) => [...addedNodes])
+    .filter(addedNode => addedNode instanceof Element && addedNode.matches(titleSelector))
+    .forEach(updateTitle)
+);
+
+export const main = async () => {
+  document.head.prepend(customTitleElement);
+  document.documentElement.append(styleElement);
+
+  observer.observe(document.head, { childList: true });
+  updateTitle(document.head.querySelector(titleSelector));
+};
+
+export const clean = async () => {
+  observer.disconnect();
+
+  customTitleElement.remove();
+  styleElement.remove();
+};
