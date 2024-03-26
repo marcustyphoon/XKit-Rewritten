219dca49e63baebdf601cb8377d6247ba61afeeb
Marcus <marcustyphoon@gmail.com>
2024-03-26 15:42:13 -0700

    refactor progress

--- src/scripts/panorama.js
+++ src/scripts/panorama.js
@@ -6,97 +6,80 @@ import { getPreferences } from '../util/preferences.js';
 const navigationWrapperBasis = 240;
 const navigationWrapperMargin = 20;
 const mainContentWrapperBasis = 966;
+const mainStyleMinWidth = navigationWrapperBasis + navigationWrapperMargin + mainContentWrapperBasis;
 
-const styleElement = buildStyle();
+const sidebarMaxWidth = 320;
+const mainRightPadding = 20;
+const mainRightBorder = 1;
+const sidebarOffset = sidebarMaxWidth + mainRightPadding + mainRightBorder;
 
-const mainContentWrapper = `${keyToCss('mainContentWrapper')}:not(${keyToCss('mainContentIsMasonry', 'mainContentIs4ColumnMasonry')})`;
-const container = `${mainContentWrapper} > ${keyToCss('container')}`;
-const mainElement = `${container} > ${keyToCss('main')}`;
-const mainPostColumn = `${mainElement} > ${keyToCss('postColumn', 'postsColumn')}`;
+const maxPostWidthVar = '--panorama-post-width';
+const aspectRatioVar = '--panorama-aspect-ratio';
+
+const createPostStyles = postColumn => `
+${postColumn}
+  :is(
+    ${keyToCss('cell')},
+    article,
+    article > header,
+    article ${keyToCss('reblog', 'videoBlock', 'audioBlock', 'link')}
+  ) {
+  max-width: 100%;
+}
+
+${postColumn} article [role="link"] > a > ${keyToCss('withImage')} {
+  height: unset;
+  aspect-ratio: 2;
+}
+${postColumn} article ${keyToCss('videoBlock')} iframe {
+  max-width: none !important;
+}
+${postColumn}
+  :is(
+    [data-is-resizable="true"][style="width: 540px;"],
+    ${keyToCss('takeoverBanner')}
+  ) {
+  width: unset !important;
+}
+${keyToCss('queueSettings')} {
+  box-sizing: border-box;
+  width: 100%;
+}
+iframe[style*="${aspectRatioVar}"] {
+  aspect-ratio: var(${aspectRatioVar});
+  height: unset !important;
+}
+`;
+
+const mainContentWrapper = `${keyToCss('mainContentWrapper')}:not(${keyToCss('mainContentIsMasonry', 'mainContentIsFullWidth')})`;
+const container = `${mainContentWrapper} > div`;
+const mainElement = `${container} > div`;
+const mainPostColumn = `${mainElement} > main${keyToCss('postColumn', 'postsColumn')}`;
+
+const mainStyleElement = buildStyle(`
+${mainContentWrapper} {
+  flex-grow: 1;
+  max-width: calc(var(${maxPostWidthVar}) + ${sidebarOffset}px);
+}
+${container} {
+  max-width: unset;
+}
+${mainElement} {
+  max-width: calc(100% - ${sidebarOffset}px);
+}
+${mainPostColumn} {
+  max-width: 100%;
+}
+` + createPostStyles(mainPostColumn));
+mainStyleElement.media = `(min-width: ${mainStyleMinWidth}px)`;
+
 const patioPostColumn = `[id]${keyToCss('columnWide')}`;
 
-const togglePanorama = async () => {
-  const hasMainColumn = document.querySelector(mainPostColumn);
-  const hasPatioColumn = document.querySelector(patioPostColumn);
-
-  if (hasMainColumn || hasPatioColumn) {
-    styleElement.media = hasPatioColumn
-      ? ''
-      : `(min-width: ${navigationWrapperBasis + navigationWrapperMargin + mainContentWrapperBasis}px)`;
-
-    const { maxPostWidth: maxPostWidthString } = await getPreferences('panorama');
-    const maxPostWidth = Number(maxPostWidthString.trim().replace('px', '')) || 0;
-
-    const sidebarMaxWidth = 320;
-    const mainRightPadding = 20;
-    const mainRightBorder = 1;
-    const sidebarOffset = sidebarMaxWidth + mainRightPadding + mainRightBorder;
-
-    const postColumn = hasPatioColumn ? patioPostColumn : mainPostColumn;
-
-    styleElement.textContent = hasPatioColumn
-      ? `
-        ${postColumn} {
-          width: min(${Math.max(maxPostWidth, 540)}px, 100vw);
-        }
-      `
-      : `
-        ${mainContentWrapper} {
-          flex-grow: 1;
-          max-width: ${Math.max(maxPostWidth, 540) + sidebarOffset}px;
-        }
-        ${container} {
-          max-width: unset;
-        }
-        ${mainElement} {
-          max-width: calc(100% - ${sidebarOffset}px);
-        }
-        ${postColumn} {
-          max-width: 100%;
-        }
-      `;
-
-    styleElement.textContent += `
-      ${postColumn}
-        :is(
-          ${keyToCss('cell')},
-          article,
-          article > header,
-          article ${keyToCss('reblog', 'videoBlock', 'audioBlock', 'link')}
-        ) {
-        max-width: 100%;
-      }
-
-      ${postColumn} article ${keyToCss('link')} ${keyToCss('header')}${keyToCss('withImage')} {
-        height: unset;
-        aspect-ratio: 2;
-      }
-      ${postColumn} article ${keyToCss('videoBlock')} iframe {
-        max-width: none !important;
-      }
-      ${postColumn}
-        :is(
-          [data-is-resizable="true"][style="width: 540px;"],
-          ${keyToCss('takeoverBanner')}
-        ) {
-        width: unset !important;
-      }
-      ${keyToCss('queueSettings')} {
-        box-sizing: border-box;
-        width: 100%;
-      }
-      iframe[style*="${aspectRatioVar}"] {
-        aspect-ratio: var(${aspectRatioVar});
-        height: unset !important;
-      }
-    `;
-    document.documentElement.append(styleElement);
-  } else {
-    styleElement.remove();
-  }
-};
-
-const aspectRatioVar = '--panorama-aspect-ratio';
+const patioStyleElement = buildStyle(`
+${patioPostColumn} {
+  width: min(var(${maxPostWidthVar}), 100vw);
+}
+` + createPostStyles(patioPostColumn));
 
 const processVideoIframes = iframes => iframes.forEach(iframe => {
   const { maxWidth, height } = iframe.style;
@@ -109,10 +92,15 @@ const processVideoIframes = iframes => iframes.forEach(iframe => {
 });
 
 export const onStorageChanged = async (changes, areaName) =>
-  Object.keys(changes).some(key => key.startsWith('panorama')) && togglePanorama();
+  Object.keys(changes).some(key => key.startsWith('panorama')) && main();
 
 export const main = async () => {
-  pageModifications.register(`:is(${mainContentWrapper}, ${mainPostColumn}, ${patioPostColumn})`, togglePanorama);
+  const { maxPostWidth: maxPostWidthString } = await getPreferences('panorama');
+  const maxPostWidth = Number(maxPostWidthString.trim().replace('px', '')) || 0;
+
+  document.body.style.setProperty(maxPostWidthVar, `${Math.max(maxPostWidth, 540)}px`);
+
+  document.documentElement.append(mainStyleElement, patioStyleElement);
 
   pageModifications.register(
     `${keyToCss('videoBlock')} iframe[style*="max-width"][style*="height"]`,
@@ -121,12 +109,13 @@ export const main = async () => {
 };
 
 export const clean = async () => {
-  pageModifications.unregister(togglePanorama);
-
   pageModifications.unregister(processVideoIframes);
   [...document.querySelectorAll(`iframe[style*="${aspectRatioVar}"]`)].forEach(el =>
     el.style.removeProperty(aspectRatioVar)
   );
 
-  styleElement.remove();
+  document.body.style.removeProperty(maxPostWidthVar);
+
+  mainStyleElement.remove();
+  patioStyleElement.remove();
 };
