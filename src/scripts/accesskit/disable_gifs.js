c756aa8f82ae4dfd3253afff5d63c128f661f15f
Marcus <marcustyphoon@gmail.com>
2024-05-22 15:39:04 -0700

    disable-gifs-blog-cards

--- src/scripts/accesskit/disable_gifs.js
+++ src/scripts/accesskit/disable_gifs.js
@@ -44,6 +44,10 @@ const styleElement = buildStyle(`
   font-size: 0.6rem;
 }
 
+${keyToCss('background')} > .${labelClass} {
+  display: none;
+}
+
 .${canvasClass} {
   position: absolute;
   visibility: visible;
@@ -151,6 +155,9 @@ const processRows = function (rowsElements) {
   });
 };
 
+const processRecommendedBlogCards = cards =>
+  cards.forEach(card => card.classList.add(containerClass));
+
 export const main = async function () {
   document.documentElement.append(styleElement);
 
@@ -162,6 +169,11 @@ export const main = async function () {
   pageModifications.register(gifBackgroundImage, processBackgroundGifs);
 
   pageModifications.register(
+    `${keyToCss('listTimelineObject')} ${keyToCss('carouselWrapper')} ${keyToCss('postCard')}`,
+    processRecommendedBlogCards
+  );
+
+  pageModifications.register(
     `:is(${postSelector}, ${keyToCss('blockEditorContainer')}) ${keyToCss('rows')}`,
     processRows
   );
@@ -171,6 +183,7 @@ export const clean = async function () {
   pageModifications.unregister(processGifs);
   pageModifications.unregister(processBackgroundGifs);
   pageModifications.unregister(processRows);
+  pageModifications.unregister(processRecommendedBlogCards);
 
   [...document.querySelectorAll(`.${containerClass}`)].forEach(wrapper =>
     wrapper.replaceWith(...wrapper.children)
