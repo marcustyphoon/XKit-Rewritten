eb9b0c558ff4be6e382fd43df5e41ab9508ed5c4
Marcus <marcustyphoon@gmail.com>
2024-11-10 18:53:34 -0800

    initial commit

---
+++ standalone/overrides/main_world/api_fetch.js
@@ -0,0 +1,31 @@
+export default function apiFetch (resource, init = {}) {
+  // convert all keys in the body to snake_case
+  if (init.body !== undefined) {
+    const objects = [init.body];
+
+    while (objects.length !== 0) {
+      const currentObjects = objects.splice(0);
+
+      currentObjects.forEach(obj => {
+        Object.keys(obj).forEach(key => {
+          const snakeCaseKey = key
+            .replace(/^[A-Z]/, match => match.toLowerCase())
+            .replace(/[A-Z]/g, match => `_${match.toLowerCase()}`);
+
+          if (snakeCaseKey !== key) {
+            obj[snakeCaseKey] = obj[key];
+            delete obj[key];
+          }
+        });
+      });
+
+      objects.push(
+        ...currentObjects
+          .flatMap(Object.values)
+          .filter(value => value instanceof Object)
+      );
+    }
+  }
+
+  return window.tumblr.apiFetch(resource, init);
+}
