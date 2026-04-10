3becf3d3f635f9066496082ddb4dbacdadd37faf
marcustyphoon <marcustyphoon@gmail.com>
2026-04-10 07:04:29 -0700

    modify script

--- dev/release-notes.js
+++ dev/release-notes.js
@@ -1,27 +1,29 @@
-#!/usr/bin/env node
+#!/usr/bin/env deno
+/// <reference types="npm:@types/node" />
+// deno-lint-ignore-file no-import-prefix
 
 import { execSync } from 'node:child_process';
+import { Octokit } from 'npm:octokit@5.0.5';
 
 const {
   GITHUB_API_URL = 'https://api.github.com',
   GITHUB_REPOSITORY = 'AprilSylph/XKit-Rewritten',
   GITHUB_TOKEN,
-} = process.env;
+} = Deno.env as unknown as Record<string, string>;
+
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
 
 try {
+  const octokit = new Octokit({ auth: GITHUB_TOKEN, baseUrl: GITHUB_API_URL });
+
   const latestTag = execSync('git describe --tags --abbrev=0', { encoding: 'utf8' }).trim();
   const refs = new Set(execSync(`git log ${latestTag}..HEAD --reverse --pretty --format="%H" --follow src/`, { encoding: 'utf8' }).trim().split('\n'));
 
-  const response = await fetch(
-    `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/commits?per_page=100`,
-    { headers: { Accept: 'application/vnd.github+json', 'X-GitHub-Api-Version': '2026-03-10', ...GITHUB_TOKEN && { Authorization: `Bearer ${GITHUB_TOKEN}` } } },
+  const commits = new Map(
+    await octokit.rest.repos
+      .listCommits({ owner, repo, per_page: 100 })
+      .then(({ data }) => data.map(commit => [commit.sha, commit])),
   );
-  if (!response.ok) { throw new Error(`🛑 HTTP ${response.status} (${response.statusText})`); }
-
-  const commits = new Map();
-  for (const commit of await response.json()) {
-    commits.set(commit.sha, commit);
-  }
 
   if (!refs.isSubsetOf(commits)) {
     console.log('> [!WARNING]');
@@ -31,14 +33,14 @@ try {
 
   console.log('```md');
   for (const ref of refs.intersection(commits)) {
-    const { author, commit } = commits.get(ref);
+    const { author, commit } = commits.get(ref)!;
 
     console.log(`- ${
       ref
     } ${
-      author.type === 'User'
-        ? `@${author.login}`
-        : `**[${author.login}](${author.html_url})**`
+      author?.type === 'User'
+        ? `@${author?.login}`
+        : `**[${author?.login}](${author?.html_url})**`
     } ${
       commit.message.includes('\n')
         ? commit.message.slice(0, commit.message.indexOf('\n'))
