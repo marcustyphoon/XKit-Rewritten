7e8aa91ad6505c79290bf5d04f46cf14a55cc66b
marcustyphoon <marcustyphoon@gmail.com>
2026-09-09 16:36:54 -0700

    Update migrate-jquery-cleanup.js

--- dev/migrate-jquery-cleanup.js
+++ dev/migrate-jquery-cleanup.js
@@ -3,6 +3,12 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
+import { ESLint } from 'eslint';
+
+// npm i -D eslint-plugin-unused-imports
+import unusedImports from 'eslint-plugin-unused-imports';
+
+const eslint = new ESLint({ fix: true, overrideConfigFile: true, overrideConfig: [{ plugins: { 'unused-imports': unusedImports }, rules: { 'no-unused-vars': 'off', 'unused-imports/no-unused-imports': 'error' } }] });
 
 const regex = (string, loose = false) => new RegExp(
   RegExp.escape(string)
@@ -11,9 +17,9 @@ const regex = (string, loose = false) => new RegExp(
   'g',
 );
 
-fs.globSync('src/features/**/*.js').forEach(filePath => {
+fs.globSync('src/features/**/*.js').forEach(async filePath => {
   const contents = fs.readFileSync(filePath, 'utf8');
-  const newContents = contents
+  let newContents = contents
     .replaceAll(regex("$('#extract').remove();"), "removeElementsById('$1');")
     .replaceAll(regex('$(`#${extract}`).remove();'), 'removeElementsById($1);')
 
@@ -31,11 +37,14 @@ fs.globSync('src/features/**/*.js').forEach(filePath => {
     .replaceAll(regex('$(extract).remove();', true), 'removeElementsBySelector($1);');
 
   if (newContents !== contents) {
+    newContents =
+    `import { removeAttribute, removeChildrenByAttribute, removeClassName, removeElementsByAttribute, removeElementsByClassName, removeElementsById, removeElementsBySelector } from '${path.relative(path.dirname(filePath), 'src/utils/cleanup.js')}';` +
+        '\n' +
+        newContents;
+
     fs.writeFileSync(
       filePath,
-      `import { removeAttribute, removeChildrenByAttribute, removeClassName, removeElementsByAttribute, removeElementsByClassName, removeElementsById, removeElementsBySelector } from '${path.relative(path.dirname(filePath), 'src/utils/cleanup.js')}';` +
-        '\n' +
-        newContents,
+      (await eslint.lintText(newContents))[0].output,
       { flag: 'w+' },
     );
     // childProcess.execFileSync('npx', ['organize-imports-cli', filePath], { stdio: 'inherit' });
